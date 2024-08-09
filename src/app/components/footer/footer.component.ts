608881391580e98b272c0dec44a0d45385499a8b
spiowm <oleksiitatar@gmail.com>
2024-08-09 13:59:14 +0300

    footer component completed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+    imports: [
+        NgIf,
+        NgOptimizedImage
+    ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
